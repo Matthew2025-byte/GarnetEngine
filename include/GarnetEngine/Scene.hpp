f8cdfee3e7d563e0b896e0a53cca7a617ae195b6
Matthew2025-byte <mdstapleton2020@outlook.com>
2026-08-29 14:04:48 -0700

    Multithreading Prototype

--- include/GarnetEngine/Scene.hpp
+++ include/GarnetEngine/Scene.hpp
@@ -1,58 +1,85 @@
+/**
+ * @file Scene.hpp
+ * @author Matthew 5-byte
+ * @brief Contains Class Scene for coordinating level specific assets and logic
+ *
+ */
+
 #pragma once
+#include <string>
+#include <vector>
 #include "Registry.hpp"
 #include "Renderer.hpp"
 
 namespace Garnet {
-    class Scene {
-    public:
-        /**
-         * @brief Runs all bound methods
-         * 
-         * @param dt deltaTime for physics updates
-         */
-        void update(float dt);
-        void save(const std::string& path) const;
-        
-        /**
-         * @brief Binds a method to Scene.update(dt)
-         * @details Argument types are automatically determined at compile time
-         * 
-         * @param func Function to bind matching (float dt, Components&...)
-         */
-        template <typename... Components>
-        void bind(void (*func)(float, Entity, Components&...)) {
-            callbacks.emplace_back([this, func](float dt) {
-                registry.each<Components...>([func, dt](Entity e, Components&... c) {
-                    func(dt, e, c...);
-                });
-            });
-        }
+/**
+ * @brief Object returned by Scene.getRequiredAssets();
+ * 
+ */
+struct usedAssets {
+	std::vector<std::string> textures;
+};
+enum assetType {
+	Texture,
+};
 
+/**
+ * @brief Class representative of a single game level
+ *
+ * Scene is a class that coordinates all necessary assets needed to properly run a user defined
+ * level.  It does not “own” any major parts of the engine, but contains lists of all assets and
+ * components needed for its specific level.  It coordinates with asset managers to load necessary
+ * information.It also keeps track of the game savestate (Non-hardcoded variables, unique to a save
+ * file).
+ */
+class Scene {
+	public:
+	/**
+	 * @brief Adds an asset to the asset list
+	 * 
+	 * @param name Name of asset to add
+	 * @param type Type (eg. Texture)
+	 */
+	void addAsset(std::string name, assetType type) {
+		switch (type) {
+			case Texture:
+				requiredAssets.textures.push_back(name);
+		}
+	};
+	/**
+	 * @brief Get the requiredAssets object
+	 * 
+	 * @return A list of all assets required to properly handle the scene
+	 */
+	usedAssets getRequiredAssets() { return requiredAssets; }
 
-        /**
-         * @brief Adds an Entity/Component pair to a Component Pool
-         * 
-         * If the component pool does not exist is is automatically
-         * created and an entity remover is registered
-         * 
-         * @tparam T Type of component
-         * @param entity Entity to assign to
-         * @param component Reference to the component to store
-         */
-        template <typename T>
-        void addComponent(Entity entity, const T& component) {
-            registry.addComponent<T>(entity, component);
-        }
+	/**
+	 * @brief Runs all bound methods
+	 *
+	 * @param dt deltaTime for physics updates
+	 */
+	void update(float dt, Registry& registry);
 
-        /**
-         * @brief Gets the internal registry object
-         * @returns Reference to Scene->Registry
-         */
-        Registry& getRegistry() { return registry; }
-        const Registry& getRegistry() const { return registry; }
+	/**
+	 * @brief Binds a method to Scene.update(dt)
+	 * @details Argument types are automatically determined at compile time
+	 *
+	 * @param func Function to bind matching (float dt, Components&...)
+	 */
+	template <typename... Components>
+	void bind(void (*func)(float, Entity, Components&...)) {
+		callbacks.emplace_back([func](float dt, Registry& registry) {
+			registry.each<Components...>(
+				[func, dt](Entity e, Components&... c) { func(dt, e, c...); });
+		});
+	}
 
-    private:
-        Registry registry;
-        std::vector<std::function<void(float)>> callbacks;
-    };
-}
+	std::vector<std::function<void(float, Registry&)>>& getCallbacks() { return callbacks; }
+	Registry& getInitRegistry() { return initRegistry; }
+
+	private:
+	std::vector<std::function<void(float, Registry&)>> callbacks;
+	usedAssets requiredAssets;
+	Registry initRegistry;
+};
+}  // namespace Garnet
