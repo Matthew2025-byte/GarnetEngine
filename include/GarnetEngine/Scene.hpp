3f6e7bd9864bfd0119e9340ef3923ebd4c378e68
Matthew2025-byte <mdstapleton2020@outlook.com>
2026-09-01 13:21:15 -0700

    Add system binding

--- include/GarnetEngine/Scene.hpp
+++ include/GarnetEngine/Scene.hpp
@@ -8,13 +8,14 @@
 #pragma once
 #include <string>
 #include <vector>
+
 #include "Registry.hpp"
 #include "Renderer.hpp"
 
 namespace Garnet {
 /**
  * @brief Object returned by Scene.getRequiredAssets();
- * 
+ *
  */
 struct usedAssets {
 	std::vector<std::string> textures;
@@ -36,7 +37,7 @@ class Scene {
 	public:
 	/**
 	 * @brief Adds an asset to the asset list
-	 * 
+	 *
 	 * @param name Name of asset to add
 	 * @param type Type (eg. Texture)
 	 */
@@ -48,7 +49,7 @@ class Scene {
 	};
 	/**
 	 * @brief Get the requiredAssets object
-	 * 
+	 *
 	 * @return A list of all assets required to properly handle the scene
 	 */
 	usedAssets getRequiredAssets() { return requiredAssets; }
@@ -62,7 +63,9 @@ class Scene {
 
 	/**
 	 * @brief Binds a method to Scene.update(dt)
-	 * @details Argument types are automatically determined at compile time
+	 * Argument types are automatically determined at compile time.
+	 * Passes deltatime (dt), the currently selected entity, and a reference to the requested
+	 * components bound to the entity.
 	 *
 	 * @param func Function to bind matching (float dt, Components&...)
 	 */
@@ -71,6 +74,24 @@ class Scene {
 		callbacks.emplace_back([func](float dt, Registry& registry) {
 			registry.each<Components...>(
 				[func, dt](Entity e, Components&... c) { func(dt, e, c...); });
+		});
+	}
+
+	/**
+	 * @brief Binds a system to the scene
+	 *
+	 * Passes deltatime (dt), the registry, and a vector of entities that matches the given filter
+	 * to the provided system. An example use case would be binding a collision detection system,
+	 * where you need access to all entities at once.
+	 *
+	 * @tparam Components The filter used to select entities
+	 * @param func System to bind matching the signature (float dt, Registry& r, const std::vector<Entity>& e)
+	 */
+	template <typename... Components>
+	void bindSystem(void (*func)(float, Registry&, const std::vector<Entity>&)) {
+		callbacks.emplace_back([func](float dt, Registry& registry) {
+			auto entities = registry.getEntities<Components...>();
+			func(dt, registry, entities);
 		});
 	}
 
